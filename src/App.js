eea1fc1d9bf6092ee8079cc343ef2db03841606f
keldibekovaa <115090119+keldibekovaa@users.noreply.github.com>
2023-02-11 12:15:48 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+ import React from 'react';
+ import Parents from './Parents';
+ 
+ 
+ function App() {
+   return (
+     <div className="App">
+        <Parents name="Ali"
+        surname="Ozturk" 
+        age="16 years">
+       
+        </Parents>
+ 
+     </div>
+   );
+ }
+ <Parents/>
+ 
+ export default App;
---
+++ src/Parents.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Parents(props) {
+    return (
+      <div>
+        {props.children}
+         {props.name}
+         {props.surname}
+         {props.age}
+      </div>
+    );
+  }
+  
+  export default Parents;
